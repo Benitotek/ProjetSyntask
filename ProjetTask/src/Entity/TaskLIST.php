2200c35c19df701c136a08e6e0754c8609138e15
Hermann <campistronherman@gmail.com>
2025-06-17 12:48:42 +0200

    maj tasklist

--- ProjetTask/src/Entity/TaskLIST.php
+++ ProjetTask/src/Entity/TaskLIST.php
@@ -8,7 +8,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
-use App\Enum\TaskListColor;
 use App\Enum\TaskListColor as EnumTaskListColor;
 
 #[ORM\Entity(repositoryClass: TaskListRepository::class)]
